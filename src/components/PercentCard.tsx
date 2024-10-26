feafe705acd633cfcd939002db5b2a8e103cbb08
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2024-10-26 09:09:35 -0300

    add Button

--- src/components/PercentCard.tsx
+++ src/components/PercentCard.tsx
@@ -4,7 +4,9 @@ import { View, Text } from "react-native";
 export function PercentCard() {
   return (
     <View className="w-full h-36 bg-[#E5F0DB] rounded-lg flex-col items-center justify-center">
-      <ArrowUpRight color="#639339" size={25} />
+      <View className="absolute top-4 right-4">
+        <ArrowUpRight color="#639339" size={25} />
+      </View>
       <Text className="font-bold text-3xl">90,86%</Text>
       <Text className="text-lg text-[#333638]">
         das refeições dentro da dieta
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,9 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Header } from "../../components/Header";
-import { Text } from "react-native";
+import { Pressable, Text } from "react-native";
 import { PercentCard } from "@/components/PercentCard";
+import { Plus, View } from "lucide-react-native";
+import { Button } from "../../../components/Button";
 
 export function Home() {
   return (
@@ -9,11 +11,21 @@ export function Home() {
       style={{
         backgroundColor: "#fff",
         height: "100%",
+        width: "100%",
+        flex: 1,
         padding: 24,
       }}
     >
       <Header />
       <PercentCard />
+
+      <Text className="text-lg mt-10 mb-2">Refeições</Text>
+      <Pressable className="w-full bg-gray-800 h-16 rounded-md justify-center items-center flex-row gap-2">
+        <Plus size={24} color="#fafafa" />
+        <Text className="text-zinc-50 text-lg font-bold">Nova refeição</Text>
+      </Pressable>
+
+      <Text className="text-lg mt-10 mb-2">Refeições</Text>
     </SafeAreaView>
   );
 }
